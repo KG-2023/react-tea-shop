a5491c6b59e67a4f2b769b38ded2ca72529d745b
Wilmer Juntado <programforwilmer@gmail.com>
2023-09-13 19:35:07 +0800

    Updated classname attribute based on react

--- src/App.js
+++ src/App.js
@@ -4,49 +4,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div class="teaMenu" >
+      <div className="teaMenu">
         <header>
-            <h1 class="animate__animated animate__backInLeft" >RL Teashop</h1>
-            <p class="established">Service since 2022</p>
+            <h1 className="animate__animated animate__backInLeft" >RL Teashop</h1>
+            <p className="established">Service since 2022</p>
         </header>
-        <hr class="top-line" />
+        <hr className="top-line" />
         <main>
            
             <section>
                 <h2>Tea</h2>
-                <article class="teaItem">
-                    <p class="teaFlavor">Aloe Tea</p><p class="teaPrice">&#x20B1; 82.00</p>
+                <article className="teaItem">
+                    <p className="teaFlavor">Aloe Tea</p><p className="teaPrice">&#x20B1; 82.00</p>
                 </article>
-                <article class="teaItem">
-                    <p class="teaFlavor">Apply Flower Tea</p><p class="teaPrice">&#x20B1; 77.00</p>
+                <article className="teaItem">
+                    <p className="teaFlavor">Apply Flower Tea</p><p className="teaPrice">&#x20B1; 77.00</p>
                 </article>
-                <article class="teaItem">
-                    <p class="teaFlavor">Assam Black Tea</p><p class="teaPrice">&#x20B1; 110.00</p>
+                <article className="teaItem">
+                    <p className="teaFlavor">Assam Black Tea</p><p className="teaPrice">&#x20B1; 110.00</p>
                 </article>
-                <article class="teaItem">
-                    <p class="teaFlavor">Broken Orange Pekoe</p><p class="teaPrice">&#x20B1; 83.00</p>
+                <article className="teaItem">
+                    <p className="teaFlavor">Broken Orange Pekoe</p><p className="teaPrice">&#x20B1; 83.00</p>
                 </article>
-                <article class="teaItem">
-                    <p class="teaFlavor">Bitter Buckwheat Tea</p><p class="teaPrice">&#x20B1; 109.00</p>
+                <article className="teaItem">
+                    <p className="teaFlavor">Bitter Buckwheat Tea</p><p className="teaPrice">&#x20B1; 109.00</p>
                 </article>
             </section>
           
             <section>
                 <h2>Tea Collection</h2>
-                <article class="teaItem">
-                    <p class="teaCollection">Black Tea</p><p class="collectionPrice">&#x20B1; 221</p>
+                <article className="teaItem">
+                    <p className="teaCollection">Black Tea</p><p className="collectionPrice">&#x20B1; 221</p>
                 </article>
-                <article class="teaItem">
-                    <p class="teaCollection">Green Tea</p><p class="collectionPrice">&#x20B1; 96</p>
+                <article className="teaItem">
+                    <p className="teaCollection">Green Tea</p><p className="collectionPrice">&#x20B1; 96</p>
                 </article>
-                <article class="teaItem">
-                    <p class="teaCollection">Oolong Tea</p><p class="collectionPrice">&#x20B1; 120</p>
+                <article className="teaItem">
+                    <p className="teaCollection">Oolong Tea</p><p className="collectionPrice">&#x20B1; 120</p>
                 </article>
-                <article class="teaItem">
-                    <p class="teaCollection">Puerh Tea</p><p class="collectionPrice">&#x20B1; 130</p>
+                <article className="teaItem">
+                    <p className="teaCollection">Puerh Tea</p><p className="collectionPrice">&#x20B1; 130</p>
                 </article>
             </section>
-            <hr class="bottom-line" />
+            <hr className="bottom-line" />
             <footer>
                 <p><a href="https://kg-2023.github.io/react-tea-shop/">Visit our website</a></p>
                 <p>0001 Marilao, Bulacan</p>
