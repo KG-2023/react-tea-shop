4d8fcdbe5afb06ccd3290cce6bc074d23a5e384a
Wilmer Juntado <programforwilmer@gmail.com>
2023-09-13 20:44:16 +0800

    TO be modified

---
+++ src/components/TestSectionsComponent.js
@@ -0,0 +1,36 @@
+const TestSectionsComponent = () => {
+    return (<>
+    <section>
+    <h2>Tea</h2>
+    <article className="teaItem">
+        <p className="teaFlavor">Aloe Tea</p><p className="teaPrice">&#x20B1; 82.00</p>
+    </article>
+    <article className="teaItem">
+        <p className="teaFlavor">Apply Flower Tea</p><p className="teaPrice">&#x20B1; 77.00</p>
+    </article>
+    <article className="teaItem">
+        <p className="teaFlavor">Assam Black Tea</p><p className="teaPrice">&#x20B1; 110.00</p>
+    </article>
+    <article className="teaItem">
+        <p className="teaFlavor">Broken Orange Pekoe</p><p className="teaPrice">&#x20B1; 83.00</p>
+    </article>
+    <article className="teaItem">
+        <p className="teaFlavor">Bitter Buckwheat Tea</p><p className="teaPrice">&#x20B1; 109.00</p>
+    </article>
+</section>        
+<section>
+    <h2>Tea Collection</h2>
+    <article className="teaItem">
+        <p className="teaCollection">Black Tea</p><p className="collectionPrice">&#x20B1; 221</p>
+    </article>
+    <article className="teaItem">
+        <p className="teaCollection">Green Tea</p><p className="collectionPrice">&#x20B1; 96</p>
+    </article>
+    <article className="teaItem">
+        <p className="teaCollection">Oolong Tea</p><p className="collectionPrice">&#x20B1; 120</p>
+    </article>
+    <article className="teaItem">
+        <p className="teaCollection">Puerh Tea</p><p className="collectionPrice">&#x20B1; 130</p>
+    </article>
+</section></>)
+}
