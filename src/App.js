0275ef38e5e703ef5e48dda83deaf40a35fecd78
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-13 21:04:59 +0800

    Add material ui icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
+import AppBar from './components/AppBar';
 import Header from './components/Header';
 import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
+      <AppBar />
       <div className="teaMenu">
+    
         <Header/>
         <hr className="top-line" />
         <Main/>  
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
-import Footer from "./Footer";
-import TestSectionsComponent from "./TestSectionsComponent";
 
+import TestSectionsComponent from "./TestSectionsComponent";
+import Footer from "./Footer";
 const Main = () => {
     return <main>                       
         <TestSectionsComponent />
